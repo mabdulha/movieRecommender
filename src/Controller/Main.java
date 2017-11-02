736da80bf0b048d4b5ac86e1de7b616c7c4e1e6b
mabdulha <mozeebabdulha@gmail.com>
2017-11-02 17:42:14 +0000

    Added Cliche

--- src/Controller/Main.java
+++ src/Controller/Main.java
@@ -3,10 +3,12 @@ package Controller;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.ObjectOutputStream;
+import java.util.Collection;
 import java.util.Scanner;
 
 import Utils.Serialiser;
 import asg.cliche.Command;
+import asg.cliche.Param;
 
 import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.io.xml.DomDriver;
@@ -33,9 +35,47 @@ public class Main {
 	  else 
 	  {
 	 movieRecommender.prime();
-	 }
-	  
-	  
-	  
+	 } 
+	}
+	
+	@Command(description = "Add a new User")
+	public void createUser(@Param(name = "First Name") String firstName,
+		@Param(name= "Last Name") String lastName,
+		@Param(name = "Age") int age,
+		@Param(name= "Gender") String gender,
+		@Param(name= "Occupation") String occupation) {
+		movieRecommender.addUser(firstName, lastName, age, gender, occupation);
+	}
+	
+	@Command(description = "Display Users")
+	public void getUser() {
+		Collection<User> users = movieRecommender.getUsers();
+		System.out.println(users);
+	}
+	
+	@Command(description = "Delete a User")
+	public void deleteUser(@Param(name = "User Id") Long userId) {
+		User user = movieRecommender.getUser(userId);
+		movieRecommender.removeUser(user);
+	}
+	
+	
+	@Command(description = "Add Movie")
+	public void addMovie(@Param(name = "title") String title,
+		@Param(name = "year") int year, 
+		@Param(name = "url") String url) {
+		movieRecommender.addMovie(title, year, url);
+	}
+	
+	@Command(description = "Display Movies")
+	public void getMovie() {
+		Collection<Movie> movies = movieRecommender.getMovies();
+		System.out.println(movies);
+	}
+	
+	@Command(description = "Delete a Movie")
+	public void deleteMovie(@Param(name = "Movie Id") Long movieId) {
+		Movie movie = movieRecommender.getMovie(movieId);
+		movieRecommender.removeMovie(movie);
 	}
 }
